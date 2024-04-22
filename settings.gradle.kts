50d27b998cd3f95d5a622673418f2583947fb650
manhhungle2906 <127386984+manhhungle2906@users.noreply.github.com>
2024-04-22 15:26:27 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+rootProject.name = "group_20"
+include(":app")
+ 
